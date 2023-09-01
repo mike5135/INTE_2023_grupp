f2a776b86459995c93d2722186900e04d5c80b57
mike5135 <micpap@kth.se>
2023-09-01 21:34:26 +0200

    test 6

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -1,8 +1,23 @@
 package org.example;
 
 public class Counter {
+
+
+    private int value;
+
+    public void inc() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    /*
     public static void main(String[] args) {
+
 
         System.out.println("Mikael4");
     }
+    */
 }
---
+++ src/main/java/org/example/CounterTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class CounterTest {
+
+    @Test
+    void newCounterInitializedToZero() {
+        Counter c = new Counter();
+        assertEquals(0, c.getValue());
+    }
+
+    @Test
+    void IncIncreasesValue() {
+        Counter c = new Counter();
+        c.inc();
+        assertEquals(1, c.getValue());
+    }
+
+}
