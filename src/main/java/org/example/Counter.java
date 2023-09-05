4eafb2a90e5601623b1e52da565a2de6fa4d3303
doriita <dorraavila@hotmail.com>
2023-09-05 12:00:39 +0200

    i did

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -18,15 +18,7 @@ public class Counter {
     }
 
     public String toString() {
-        String l = "Hej";
-        int o = 1;
-        String l1 = "Hi + Hej";
-        int yy= 99;
-        int yy2= 999;
-        int p = 23;
-        String l2 ="HI";
-        String l3 ="HI1";
-        String l4 ="HI2";
+
         return "This is the current value 1 + 23" + value;
     }
 
